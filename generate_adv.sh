5c90c0398d6275efc83a2653f18bcb0eca06a1d3
Luke Chang <luke.x.chang@gmail.com>
2022-10-28 06:03:14 +1300

    updated Epsilon

--- generate_adv.sh
+++ generate_adv.sh
@@ -20,15 +20,15 @@ python ./experiments/train_adv_examples.py -d=MNIST --attack=CW2 --params='{"max
 
 # Data:CIFAR10,Attack:FGSM
 python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=FGSM --params='{"norm":"inf"}' --eps="[0.01,0.02,0.03,0.05,0.06,0.08,0.09,0.11,0.12]"
-python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=FGSM --params='{"norm":2}' --eps="[0.01,0.02,0.03,0.09,0.16,0.22,0.28,0.34,0.41,0.47,0.53]"
+python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=FGSM --params='{"norm":2}' --eps="[0.1,0.2,0.3,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2]"
 
-# Data:CIFAR10,Attack:PGD
+Data:CIFAR10,Attack:PGD
 python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=PGD --params='{"norm":"inf","eps_iter":0.01}' --eps="[0.01,0.02,0.03,0.05,0.06,0.08,0.09,0.11,0.12]"
-python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=PGD --params='{"norm":2,"eps_iter":0.01}' --eps="[0.01,0.02,0.03,0.09,0.16,0.22,0.28,0.34,0.41,0.47,0.53]"
+python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=PGD --params='{"norm":2,"eps_iter":0.05}' --eps="[0.1,0.2,0.3,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2]"
 
 # Data:CIFAR10,Attack:APGD
 python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=APGD --params='{"norm":"inf","eps_iter":0.01}' --eps="[0.01,0.02,0.03,0.05,0.06,0.08,0.09,0.11,0.12]"
-python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=APGD --params='{"norm":2,"eps_iter":0.01}' --eps="[0.01,0.02,0.03,0.09,0.16,0.22,0.28,0.34,0.41,0.47,0.53]"
+python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=APGD --params='{"norm":2,"eps_iter":0.05}' --eps="[0.1,0.2,0.3,0.4,0.6,0.8,1,1.2,1.4,1.6,1.8,2]"
 
 # Data:CIFAR10,Attack:CW2
 python ./experiments/train_adv_examples.py -d=CIFAR10 --attack=CW2 --params='{"max_iterations":200}' --eps="[0,1,10]"
