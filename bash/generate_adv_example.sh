521d5b8f49126e454e4a6ae47650302c25c40522
Luke Chang <luke.x.chang@gmail.com>
2022-11-06 12:04:16 +1300

    updated script for generating adv examples

--- bash/generate_adv_example.sh
+++ bash/generate_adv_example.sh
@@ -5,7 +5,8 @@
 source ./.venv/bin/activate
 pip install --upgrade .
 
-SEEDS=(688283 692478 882347 366364 943597)
+# SEEDS=(188283 292478 382347 466364 543597)
+SEEDS=(188283) # TODO: Run 1 repeatation first
 SIZE=1000
 
 for SEED in ${SEEDS[@]}; do
@@ -13,15 +14,15 @@ for SEED in ${SEEDS[@]}; do
     DATA=MNIST
     # Attack:APGD
     python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=APGD --params='{"norm":"inf","eps_iter":0.03}' --eps="[0.22,0.66]"
-    python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=APGD --params='{"norm":2,"eps_iter":0.1}' --eps="[4,8,12]"
+    python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=APGD --params='{"norm":2,"eps_iter":0.1}' --eps="[4,8]"
     # Attack:CW2
     python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=CW2 --params='{"max_iterations":200}' --eps="[0]"
 
     echo "Running CIFAR10 #####################################################"
     DATA=CIFAR10
     # Attack:APGD
-    python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=APGD --params='{"norm":"inf","eps_iter":0.01}' --eps="[0.01,0.1,0.3]"
-    python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=APGD --params='{"norm":2,"eps_iter":0.05}' --eps="[0.3,1.8,3]"
+    python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=APGD --params='{"norm":"inf","eps_iter":0.01}' --eps="[0.01,0.1]"
+    python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=APGD --params='{"norm":2,"eps_iter":0.05}' --eps="[0.3,3]"
     # Attack:CW2
     python ./experiments/train_adv_examples.py --seed=$SEED --n_att=$SIZE --n_val=$SIZE -d=$DATA --attack=CW2 --params='{"max_iterations":200}' --eps="[0]"
 done
