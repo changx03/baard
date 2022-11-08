733f4ab148c0c487a12b7be21eba1d1db31cb3fa
Luke Chang <luke.x.chang@gmail.com>
2022-11-09 02:51:35 +1300

    fix SEEDS is a list

--- linux/grey_box_detect.sh
+++ linux/grey_box_detect.sh
@@ -13,18 +13,20 @@ DETECTORS=("BAARD-S1" "BAARD-S2" "BAARD-S3" "BAARD" "FS" "LID" "ML-LOO" "Odds" "
 # DATASETS=("MNIST" "CIFAR10")
 DATASETS=("MNIST")  # TODO: Do NOT commit this!
 
-for DATA in ${DATASETS[@]}; do
-    for DETECTOR in ${DETECTORS[@]}; do
-        ATTACK="APGD"
-        NORMS=("inf" "2")
-        for NORM in ${NORMS[@]}; do
-            echo "Running $DETECTOR on $DATA with $ATTACK L$NORM"
-            python ./experiments/extract_features.py --s $SEEDS --data $DATA --attack $ATTACK -l $NORM --detector $DETECTOR
-        done
+for SEED in ${SEEDS[@]}; do
+    for DATA in ${DATASETS[@]}; do
+        for DETECTOR in ${DETECTORS[@]}; do
+            ATTACK="APGD"
+            NORMS=("inf" "2")
+            for NORM in ${NORMS[@]}; do
+                echo "Running $DETECTOR on $DATA with $ATTACK L$NORM"
+                python ./experiments/extract_features.py -s $SEED --data $DATA --attack $ATTACK -l $NORM --detector $DETECTOR
+            done
 
-        ATTACK="CW2"
-        NORM="2"
-        echo "Running $DETECTOR on $DATA with $ATTACK L$NORM"
-        python ./experiments/extract_features.py --s $SEEDS --data $DATA --attack $ATTACK -l $NORM --detector $DETECTOR
+            ATTACK="CW2"
+            NORM="2"
+            echo "Running $DETECTOR on $DATA with $ATTACK L$NORM"
+            python ./experiments/extract_features.py -s $SEED --data $DATA --attack $ATTACK -l $NORM --detector $DETECTOR
+        done
     done
 done
