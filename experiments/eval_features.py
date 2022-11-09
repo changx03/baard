1a2dfef644e167ebfa62ce76b510f5e8528f3188
Luke Chang <luke.x.chang@gmail.com>
2022-11-09 23:21:07 +1300

    add save ROC, AUC and TPR

--- experiments/eval_features.py
+++ experiments/eval_features.py
@@ -3,6 +3,10 @@ import logging
 import os
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import Union
+
+import torch
+from pandas import DataFrame
 
 from baard.attacks import ATTACKS, L_NORM
 from baard.classifiers import DATASETS
@@ -13,63 +17,92 @@ from baard.utils.miscellaneous import norm_parser
 logger = logging.getLogger(__name__)
 
 
-def eval_features(path_features, filename_adv_feature, filename_clean_feature, path_out):
-    """Compute ans save ROC, AUC, TPR@1FPR and TPR@5FPR."""
-    # TODO: Implement this!
-    pass
+def eval_features(path_input, path_output, file_clean, file_adv
+                  ) -> Union[DataFrame, DataFrame]:
+    """Compute ans save ROC, AUC, TPR at 1%, 5% and 10% FPR. Returns (ROC, others).
+    """
+    features_clean = torch.load(os.path.join(path_input, file_clean))
+    features_adv = torch.load(os.path.join(path_input, file_adv))
+    fpr, tpr, auc_score, thresholds = compute_roc_auc(features_clean, features_adv)
+
+    tpr_1fpr, _ = tpr_at_n_fpr(fpr, tpr, thresholds, n_fpr=0.01)
+    tpr_5fpr, _ = tpr_at_n_fpr(fpr, tpr, thresholds, n_fpr=0.05)
+    tpr_10fpr, _ = tpr_at_n_fpr(fpr, tpr, thresholds, n_fpr=0.1)
+
+    # Use `file_adv` name to store
+    filename_output = Path(file_adv).stem  # Remove extension
+    # Use DataFrame to save ROC
+    data_roc = {
+        'fpr': fpr,
+        'tpr': tpr,
+        'thresholds': thresholds,
+    }
+    df_roc = DataFrame(data_roc)
+    path_roc = os.path.join(path_output, f'{filename_output}-roc.csv')
+    df_roc.to_csv(path_roc, index=False)
+    logger.info('Save ROC to: %s', path_roc)
+
+    # Save AUC and TPR@?FPR
+    data_other = {
+        'auc': [auc_score],
+        '1fpr': [tpr_1fpr],
+        '5fpr': [tpr_5fpr],
+        '10fpr': [tpr_10fpr],
+    }
+    df_auc_tpr = DataFrame(data_other)
+    path_auc_tpr = os.path.join(path_output, f'{filename_output}-auc_tpr.csv')
+    df_auc_tpr.to_csv(path_auc_tpr, index=False)
+    logger.info('Save AUC and TPRs to: %s', path_auc_tpr)
+    return df_roc, df_auc_tpr
 
 
 def parse_arguments():
     """Parse command line arguments.
     Example:
-    python ./experiments/eval_features.py -s 1234 --data MNIST --detector FS --eps 0.5
+    python ./experiments/eval_features.py -i "./results/exp1234/MNIST/ApplicabilityStage/APGD-L2" \
+        -o "./results/exp1234/roc" \
+        --clean "ApplicabilityStage-MNIST-APGD-L2-clean" \
+        --adv "ApplicabilityStage-MNIST-APGD-L2-0.5"
     """
     parser = ArgumentParser()
-    parser.add_argument('-s', '--seed', type=int, required=True)
-    parser.add_argument('--data', type=str, choices=DATASETS, required=True)
-    parser.add_argument('--detector', type=str, choices=DETECTORS, required=True)
-    parser.add_argument('-a', '--attack', choices=ATTACKS, default='APGD')
-    parser.add_argument('-l', '--lnorm', type=str, choices=L_NORM, default='2')
-    parser.add_argument('--eps', type=str, required=True)
-    parser.add_argument('-p', '--path', type=str, default='results')
+    parser.add_argument('-i', '--input', type=str, required=True)
+    parser.add_argument('-o', '--output', type=str, required=True)
+    parser.add_argument('--clean', type=str, required=True)
+    parser.add_argument('--adv', type=str, required=True)
+
     args = parser.parse_args()
-    seed = args.seed
-    data_name = args.data
-    detector_name = args.detector
-    attack_name = args.attack
-    l_norm = args.lnorm
-    path = args.path
-    eps = float(args.eps)
+    path_input = args.input
+    path_output = args.output
+    file_clean = args.clean
+    file_adv = args.adv
 
-    l_norm = norm_parser(l_norm)
-    detector_class_name = DETECTOR_CLASS_NAMES[detector_name]
-    filename_adv_feature = f'{detector_class_name}-{data_name}-{attack_name}-{l_norm}-{eps}'
-    filename_clean_feature = f'{detector_class_name}-{data_name}-{attack_name}-{l_norm}-clean'
-    path_features = Path(
-        os.path.join(path, f'exp{seed}', data_name, detector_class_name,
-                     f'{attack_name}-{l_norm}')
-    ).absolute()
-    path_out = Path(os.path.join(path, f'exp{seed}', 'roc')).resolve()
-    if not os.path.exists(path_out):
-        logger.info('Create folder: %s', path_out)
-        os.makedirs(path_out)
+    # Check extension. Features are saved using torch.save();
+    if file_clean[-3:] != '.pt':
+        file_clean = file_clean + '.pt'
+    if file_adv[-3:] != '.pt':
+        file_adv = file_adv + '.pt'
 
-    print(' PATH FEATURES:', path_features)
-    print('   PATH OUTPUT:', path_out)
-    print('  ADV FILENAME:', filename_adv_feature)
-    print('CLEAN FILENAME:', filename_clean_feature)
+    # Check file existence.
+    for file in [file_clean, file_adv]:
+        path_file = os.path.join(path_input, file)
+        if not os.path.exists(path_file):
+            raise FileNotFoundError(f'Cannot find {path_file}')
 
-    if not (os.path.exists(os.path.join(path_features, f'{filename_adv_feature}.pt'))
-            and os.path.exists(os.path.join(path_features, f'{filename_clean_feature}.pt'))):
-        raise FileNotFoundError('Cannot find trained features!')
-    return path_features, filename_adv_feature, filename_clean_feature, path_out
+    # Check output directory existence.
+    path_output = Path(path_output).absolute()
+    if not path_output.is_dir():
+        logger.warning('Directory does not exist. Create %s', path_output)
+        os.makedirs(path_output)
+
+    return path_input, path_output, file_clean, file_adv
 
 
 def main():
     """Main pipeline for evaluating extracted features from detectors"""
-    path_features, filename_adv_feature, filename_clean_feature, path_out = parse_arguments()
-    eval_features(path_features, filename_adv_feature, filename_clean_feature, path_out)
+    path_input, path_output, file_clean, file_adv = parse_arguments()
+    eval_features(path_input, path_output, file_clean, file_adv)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     main()
