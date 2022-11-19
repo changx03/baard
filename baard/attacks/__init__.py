2aecb4d010b015dc010c5bdccdaa4edaaaa7eeed
Luke Chang <luke.x.chang@gmail.com>
2022-11-20 01:29:58 +1300

    update script for BAARD extracts whitebox features

--- baard/attacks/__init__.py
+++ baard/attacks/__init__.py
@@ -1,5 +1,5 @@
 """Constants for attacks."""
 
-ATTACKS = ['FGSM', 'PGD', 'CW2', 'APGD']
+ATTACKS = ['FGSM', 'PGD', 'CW2', 'APGD', 'whitebox']
 ATTACKS_SKLEARN = ['PGD-Linf', 'DecisionTreeAttack']
 L_NORM = ['inf', '2']
---
+++ linux/whitebox_extract_features.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script runs the ablation study for BAARD.
+# It tests BAARD on a wide range of epsilon.
+# NOTE: The adversarial examples MUST be pre-trained!
+
+source ./.venv/bin/activate
+python -m pip install --upgrade .
+
+SEED=727328
+ATTACK="whitebox"
+NORMS=("inf" "2")
+DETECTORS=("BAARD-S1" "BAARD-S2" "BAARD-S3" "BAARD")
+DATASETS=("MNIST" "CIFAR10")
+
+for DATA in ${DATASETS[@]}; do
+    for DETECTOR in ${DETECTORS[@]}; do
+        for NORM in ${NORMS[@]}; do
+            echo "Running $DETECTOR on $DATA with $ATTACK L$NORM"
+            python ./experiments/extract_features.py -s $SEED --data $DATA --attack $ATTACK -l $NORM --detector $DETECTOR
+        done
+    done
+done
