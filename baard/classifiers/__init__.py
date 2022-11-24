b843ab2945bd14b6aab1eb3ba7bfa2d135a97f70
Luke Chang <luke.x.chang@gmail.com>
2022-11-24 18:23:46 +1300

    add tabular data loopup

--- baard/classifiers/__init__.py
+++ baard/classifiers/__init__.py
@@ -8,9 +8,15 @@ from .mnist_cnn import MNIST_CNN
 
 DATASETS = ['MNIST', 'CIFAR10']
 TABULAR_DATA_LOOKUP = {
+    'abalone': os.path.join('tabular', 'abalone_preprocessed.csv'),
     'banknote': os.path.join('tabular', 'banknote_preprocessed.csv'),
     'BC': os.path.join('tabular', 'breastcancer_preprocessed.csv'),
+    'CMC': os.path.join('tabular', 'cmc_preprocessed.csv'),
     'HTRU2': os.path.join('tabular', 'htru2_preprocessed.csv'),
+    'phoneme': os.path.join('tabular', 'phoneme_preprocessed.csv'),
+    'ringnorm': os.path.join('tabular', 'ringnorm_preprocessed.csv'),
+    'texture': os.path.join('tabular', 'texture_preprocessed.csv'),
+    'yeast': os.path.join('tabular', 'yeast_preprocessed.csv'),
 }
 TABULAR_DATASETS = list(TABULAR_DATA_LOOKUP.keys())
 TABULAR_MODELS = ['SVM', 'DecisionTree']
