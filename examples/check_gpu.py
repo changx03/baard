4e66e8b94848cde9940609c07e3f5a3b0b0f0e31
Luke Chang <luke.x.chang@gmail.com>
2022-12-05 18:28:01 +1300

    fix check_gpu

--- examples/check_gpu.py
+++ examples/check_gpu.py
@@ -1,7 +1,8 @@
 """Check if GPU is available. """
 import torch
 
-if __name__ == '__main__':
+
+def run():
     if torch.cuda.is_available():
         device = torch.device('cuda')
 
@@ -9,13 +10,16 @@ if __name__ == '__main__':
         print('GPU Memory Usage:')
         print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024**3, 1), 'GB')
         print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024**3, 1), 'GB')
-else:
-    device = torch.device('cpu')
-    print('WARNING: GPU is not found!')
-    print(f'Running on Device: {device}')
-    exit()
+    else:
+        device = torch.device('cpu')
+        print('WARNING: GPU is not found!')
+        print(f'Running on Device: {device}')
 
-# Run a trivial task
-x = torch.rand(10).to(device)
-x = x.sum().item()
-print(f'Sum random numbers from {device}:', x)
+    # Run a trivial task
+    x = torch.rand(10).to(device)
+    x = x.sum().item()
+    print(f'Sum random numbers from {device}:', x)
+
+
+if __name__ == '__main__':
+    run()
